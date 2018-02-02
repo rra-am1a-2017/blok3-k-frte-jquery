236b397a8484e3a66724827e06ab9eff2a52c4e8
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-02 16:14:22 +0100

    Einde lesweek 1

--- selectors_and_filters/js/jquery.js
+++ selectors_and_filters/js/jquery.js
@@ -17,29 +17,28 @@ $(document).ready(function() {
          .css("padding", "1em")
          .css("font-size", "1.3em");
 
+  // We selecteren een li tag en we passen de css toe die staat in het js object style_li
   $("li").css(style_li);
 
+  // We maken een js object met drie css property - value paren
   var style_ol = {"background-color" : "rgb(200, 200, 200)",
                   "font-family" : "Verdana",
                   "text-align" : "center"};
   
+  // We selecteren de ol geven het js object met css instellingen mee
   $("ol").css(style_ol);
 
-  // Dit is een selector voor een id
+  // Dit is een selector voor een id="eerste" en we passen css toe in een js object
   $("#eerste").css({"font-size" : "1.5em"});
 
-  // Dit is een selector voor een class
+  // Dit is een selector voor een class="alert" in een ol met id="a" en passen css toe in een js object
   $("ol.a .alert").css("font-size", "5em");
 
+  // We selecteren een tag met id="eerste" en class="amazing" en passen css in js object toe
   $(".amazing#eerste").css({"font-size" : "1.8em", "border" : "5px dashed orange"});
 
+  // We selecteren alle tags op de pagina en passen css toe.
   $("*").css("font-family", "Verdana");
-
-
-
-
-
-
 });
 
 
