9258f79cc29c74fc2366a72d18891efed5889095
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-21 10:30:52 +0100

    addclass

--- changing css classes/js/jquery.js
+++ changing css classes/js/jquery.js
@@ -1,6 +1,18 @@
 // Hier wordt gekeken of de index.html pagina helemaal(behalve de plaatjes) geladen is.
 $(document).ready(function() {
-  
+  // Selecteer de button
+  $("#addclass").on("click", function() {
+    $("#eerste").addClass("fat-border");
+  });
+
+  $("#addFancyClass").on("click", function () {
+    $("#eerste").addClass("fancy-div");
+  });
+
+  // Gebruik removeClass om de class te verwijderen
+  $("#rmClass").on("click", function () {
+    $("#tweede").removeClass("roundCorners");
+  });
 });
 
 
