cdd9978e4ba5686754ce50160d0efda1ea50cb12
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-03-14 10:44:04 +0100

    ajax

--- ajax_carousel/data.php
+++ ajax_carousel/data.php
@@ -1,4 +1,17 @@
 <?php
-  $text = "Dit is wat tekst";
-  echo $text;
+  // Maak contact met een mysql-server
+  include("./connect_db.php");
+
+
+  // Selecteer alle records uit de database
+  $sql = "SELECT * FROM `pictures` WHERE `category` = '{$_POST["category"]}'";
+
+  // Vuur de query af op de database
+  $result = mysqli_query($conn, $sql);
+
+  // Maak van het result een associatief array
+  $records = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // Schrijf het naar het scherm
+  echo json_encode($records);
 ?>
--- ajax_carousel/js/script.js
+++ ajax_carousel/js/script.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
+
   $("#selectPics").on("change", function () {
     //alert("Er is iets veranderd in de select");
     var value = $(this).val();
-    //alert(value);
 
     // We gaan asynchroon data ophalen van een php pagina
     $.post("http://localhost/2017-2018/blok3/k-frte-am1a/ajax_carousel/data.php",
+            {'category' : value},
             function(data) {
-              $("#response").val(data);
+              $("#response").val(data[2].name);
+              
+              var i;
+              for (i = 0;  i < data.length; i++) {
+                $(".carousel-inner img").eq(i).attr("src", "./images/" + data[i].name);
+              }
+
             },
-           "text");
+           "json");
   });
 });
